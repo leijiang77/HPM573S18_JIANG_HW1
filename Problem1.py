4d7800b361366cea73989226e1f86fc8d4444eaa
Lei <juliejiang7@gmail.com>
2018-01-22 16:45:21 -0500

    update problem1 & problem2

---
+++ Problem1.py
@@ -0,0 +1,35 @@
+
+#problem 1.1
+
+x=17
+
+print('Setting x = ',x)
+
+#integer
+y1 = x
+print('y2 is a ',type(y1))
+print(y1)
+
+#float
+y2 = float(x)
+print('y2 is a ',type(y2))
+print(y2)
+#string
+
+y3 = str(x)
+print 'y3 is a ',type(y3)
+print(y3)
+#boolean '17>1"
+
+y4 = bool(x>1)
+print('y4 is a ',type(y4))
+print(y4)
+
+
+#problem 1.2
+
+text = "The "+ "Value " +"of " + "x " +"is " +y3+'.'
+print(text)
+
+
+
---
+++ Problem2.py
@@ -0,0 +1,5 @@
+
+#problem 2
+
+text = "I "+ "love " +"learning " + "how " +"to " + "code" + "!"
+print(text)
